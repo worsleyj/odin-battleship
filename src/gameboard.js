1edc484d2913fc6a1672d955f44b1df730b8c8f2
worsleyj <josephworsley23@gmail.com>
2024-09-03 13:04:23 -0400

    add gameboard 2d array

---
+++ src/gameboard.js
@@ -0,0 +1,19 @@
+const Ship = require("./ship");
+
+class Gameboard {
+  constructor(x, y) {
+    const coords = [];
+    for (let i = 1; i <= x; i++) {
+      coords[i] = [];
+      for (let j = 1; j <= y; j++) {
+        coords[i][j] = 0;
+      }
+    }
+  }
+  placeShip(x, y) {
+    const newShip = new Ship(2);
+  }
+  receiveAttack(x, y) {}
+}
+
+module.exports = Gameboard;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import "./reset.css";
 import "./styles.css";
 const Ship = require("./ship");
 
-const ship = new Ship(1);
-ship.hit();
-ship.hit();
-ship.hit();
-ship.hit();
-ship.hit();
-console.log(ship);
-console.log(ship.isSunk());
+class Gameboard {
+  constructor(x, y) {
+    const coords = [];
+    for (let i = 1; i <= x; i++) {
+      coords[i] = [];
+      for (let j = 1; j <= y; j++) {
+        coords[i][j] = 0;
+      }
+    }
+  }
+  placeShip(x, y) {
+    const newShip = new Ship(2);
+  }
+  receiveAttack(x, y) {}
+}
 
 console.log("TEST2");
