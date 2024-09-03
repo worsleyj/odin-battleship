f420300032113f810ed6460656f792ff0b704b81
worsleyj <josephworsley23@gmail.com>
2024-09-03 13:15:12 -0400

    add placeShip and receiveAttack

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const Ship = require("./ship");
 class Gameboard {
   constructor(x, y) {
     const coords = [];
+    this.coords = coords;
     for (let i = 1; i <= x; i++) {
       coords[i] = [];
       for (let j = 1; j <= y; j++) {
@@ -13,9 +14,24 @@ class Gameboard {
     }
   }
   placeShip(x, y) {
-    const newShip = new Ship(2);
+    const newShip = new Ship(1);
+    this.coords[y][x] = newShip;
   }
-  receiveAttack(x, y) {}
+  receiveAttack(x, y) {
+    if (this.coords[y][x] === 0) {
+      console.log("You Missed! X: " + x + ", Y: " + y);
+    } else {
+      this.coords[y][x].hit();
+    }
+  }
 }
+
+const board = new Gameboard(5, 5);
+console.log(board.coords);
+board.placeShip(3, 4);
+board.receiveAttack(3, 4);
+board.receiveAttack(4, 3);
+console.log(board.coords);
+console.log(board.coords[4][3].isSunk());
 
 console.log("TEST2");
