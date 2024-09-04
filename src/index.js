fef7cdc0fcdfdb6b266fdbaddbc9c0d2ff78101f
worsleyj <josephworsley23@gmail.com>
2024-09-04 12:55:14 -0400

    add isGameOver and create ships array

--- src/index.js
+++ src/index.js
@@ -4,25 +4,36 @@ const Ship = require("./ship");
 
 class Gameboard {
   constructor(x, y) {
-    const coords = [];
-    this.coords = coords;
+    this.coords = [];
+    this.ships = [];
     for (let i = 1; i <= x; i++) {
-      coords[i] = [];
+      this.coords[i] = [];
       for (let j = 1; j <= y; j++) {
-        coords[i][j] = 0;
+        this.coords[i][j] = 0;
       }
     }
   }
   placeShip(y, x) {
     const newShip = new Ship(1);
+    this.ships.push(newShip);
     this.coords[y][x] = newShip;
   }
   receiveAttack(y, x) {
     if (this.coords[y][x] === 0) {
+      this.coords[y][x] = "X";
       console.log("You Missed! X: " + x + ", Y: " + y);
     } else {
       this.coords[y][x].hit();
     }
+  }
+  isGameOver() {
+    let allShipsSunk = true;
+    this.ships.forEach((ship) => {
+      if (!ship.isSunk()) {
+        allShipsSunk = false;
+      }
+      console.log("AllSunk?" + allShipsSunk);
+    });
   }
 }
 
@@ -33,5 +44,5 @@ board.receiveAttack(3, 4);
 board.receiveAttack(4, 3);
 console.log(board.coords);
 console.log(board.coords[4][3].isSunk());
-
+board.isGameOver();
 console.log("TEST2");
